ced68c0bee8b39310c59b4f584fb656d7f7252ee
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-03-29 16:54:05 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,8 @@
 def introduction(name)
   puts "Hi, my name is Josh."
 end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is Dan and I am learning to program in Emb
+er.js"
+end
